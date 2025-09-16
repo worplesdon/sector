17a9b278240a43e7fc4057f7b1ddfd22a266f705
ellisbow! <ellisbowyer@yahoo.co.uk>
2025-09-16 16:09:23 +0100

    Add ValidationExceptionHandler for hanfling cases where valid data is not subbmitted to apis

---
+++ src/main/java/org/example/sector/controller/ValidationExceptionHandler.java
@@ -0,0 +1,28 @@
+package org.example.sector.controller;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ValidationExceptionHandler {
+
+  /**
+   * Handle the MethodArgumentNotValidException by getting all the validation errors into a map of
+   * the messages keyed of the field name of the offending field. The Map is return as part of a
+   * "Bad Request" ResponseEntity
+   */
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, String>> handleValidationErrors(
+      MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new LinkedHashMap<>();
+    ex.getBindingResult()
+        .getFieldErrors()
+        .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  }
+}
